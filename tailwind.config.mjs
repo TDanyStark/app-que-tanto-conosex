014429b72af9eab5fd410be1eeb6c84f5a7dddc9
Daniel Amado <daniel.amadove@gmail.com>
2024-05-21 11:45:58 -0500

    Cards y funcionalidad terminadas

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,6 +9,9 @@ export default {
 				'rosado-oscuro': '#d787b5',
 				'morado-texto': '#772583',
 			},
+			fontSize: {
+        '12xl': '12em', // Agrega esta línea para definir el tamaño de texto de 12em
+      },
 		},
 	},
 	plugins: [],
